bc16b53e8303e74b1b4f021745b8794dd0a54cb0
Long Vu <long.vudai01@gmail.com>
2023-03-16 00:27:35 +0700

    formatting

--- Sources/ColorKit/SystemColor+macOS.swift
+++ Sources/ColorKit/SystemColor+macOS.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Long Vu on 15/03/2023.
 //
@@ -8,24 +8,24 @@
 import Foundation
 
 #if os(macOS)
-import SwiftUI
-import AppKit.NSColor
+    import AppKit.NSColor
+    import SwiftUI
 
-public extension Color {
-    static var secondaryLabelColor: Color {
-        return .init(nsColor: .secondaryLabelColor)
+    public extension Color {
+        static var secondaryLabelColor: Color {
+            return .init(nsColor: .secondaryLabelColor)
+        }
+
+        static var textColor: Color {
+            return .init(nsColor: .textColor)
+        }
+
+        static var controlBackgroundColor: Color {
+            return .init(nsColor: .controlBackgroundColor)
+        }
+
+        static var controlColor: Color {
+            return .init(nsColor: .controlColor)
+        }
     }
-    
-    static var textColor: Color {
-        return .init(nsColor: .textColor)
-    }
-    
-    static var controlBackgroundColor: Color {
-        return .init(nsColor: .controlBackgroundColor)
-    }
-    
-    static var controlColor: Color {
-        return .init(nsColor: .controlColor)
-    }
-}
 #endif
