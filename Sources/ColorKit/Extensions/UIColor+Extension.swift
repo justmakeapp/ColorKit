0a4feeaec8e0038e5f5d842793966e2112a233f0
Long Vu <long.vudai01@gmail.com>
2024-10-26 18:47:22 +0700

    add color spaces

--- Sources/ColorKit/Extensions/UIColor+Extension.swift
+++ Sources/ColorKit/Extensions/UIColor+Extension.swift
@@ -74,44 +74,4 @@ import SwiftUI
         }
     }
 
-    #if os(iOS) || os(visionOS)
-        public extension UIColor {
-            func lighter(by percentage: CGFloat = 30.0) -> UIColor? {
-                let dynamicColor = UIColor { traitCollection in
-                    switch traitCollection.userInterfaceStyle {
-                    case .dark:
-                        return self.adjust(by: abs(percentage)) ?? .clear
-                    default:
-                        return self.adjust(by: abs(percentage)) ?? .clear
-                    }
-                }
-                return dynamicColor
-            }
-
-            func darker(by percentage: CGFloat = 30.0) -> UIColor? {
-                let dynamicColor = UIColor { traitCollection in
-                    switch traitCollection.userInterfaceStyle {
-                    case .dark:
-                        return self.adjust(by: -1 * abs(percentage)) ?? .clear
-                    default:
-                        return self.adjust(by: -1 * abs(percentage)) ?? .clear
-                    }
-                }
-                return dynamicColor
-            }
-
-            func adjust(by percentage: CGFloat = 30.0) -> UIColor? {
-                var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
-                if getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
-                    return UIColor(red: min(red + percentage / 100, 1.0),
-                                   green: min(green + percentage / 100, 1.0),
-                                   blue: min(blue + percentage / 100, 1.0),
-                                   alpha: alpha)
-                } else {
-                    return nil
-                }
-            }
-        }
-    #endif
-
 #endif
