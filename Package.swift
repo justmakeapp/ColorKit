05b81bb007e24a5c4b8927281c09488cec384196
Long Vu <long.vudai01@gmail.com>
2024-11-01 19:00:42 +0700

    update Swift tools version to 6.0 and remove deprecated Package.swift file

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version:6.0
 
 import PackageDescription
 
@@ -23,5 +23,6 @@ let package = Package(
     targets: [
         .target(name: "ColorKit"),
         .target(name: "ColorKitUI", dependencies: ["ColorKit"]),
-    ]
+    ],
+    swiftLanguageModes: [.v6]
 )
